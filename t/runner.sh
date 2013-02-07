760ffe3554e5839ebb9622ed01f1c8c96bd2e825
Szymon Urbaś <szymon.urbas@aol.com>
2013-02-07 15:47:46 +0000

    Update test runner.

--- t/runner.sh
+++ t/runner.sh
@@ -3,7 +3,7 @@
 # A small shell script to launch all the test files.
 # Note, it's got to be ran directly from the root directory (not 't' or anything).
 
-if [ ! -f ./bin/nemo ]; then
+if [ ! -f ./nemo ]; then
   echo "Nemo seems to be not compiled. Run \`make\`."
   exit 1
 fi
@@ -12,7 +12,7 @@ fail=0
 
 for i in t/*.nm; do
   echo "Running tests in \"`basename $i`\""
-  eval ./bin/nemo $i
+  eval ./nemo $i
   if [ $? -ne 0 ]; then
     fail=1
   fi
